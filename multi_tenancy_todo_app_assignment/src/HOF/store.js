1da5cf48280642495a8a01db8f7311d25256fb2b
suvampanda <suvam.panda@devriser.com>
2023-06-01 13:34:52 +0530

    added subtask component with all crud operations

--- multi_tenancy_todo_app_assignment/src/HOF/store.js
+++ multi_tenancy_todo_app_assignment/src/HOF/store.js
@@ -4,6 +4,7 @@ import thunk from "redux-thunk";
 import { todoReducer } from "./TodoReducer/todo.reducer";
 import { reducer as authReducer } from "./AuthReducer/auth.reducer"
 import { userReducer } from "./UserReducer/user.reducer";
-const rootreducer = combineReducers({authReducer,todoReducer,userReducer});
+import subtaskReducer from "./SubTaskReducer/SubTask.reducer";
+const rootreducer = combineReducers({authReducer,todoReducer,userReducer,subtaskReducer});
 const store = legacy_createStore(rootreducer, applyMiddleware(thunk));
 export { store };
